e2462d6fbb0b96af36ed80ed105ca7433503ddac
swapnaIM <110450026+swapnaIM@users.noreply.github.com>
2022-10-19 18:38:56 +0530

    First commit

---
+++ src/test/java/extentlisteners/ExtentManager.java
@@ -0,0 +1,56 @@
+package extentlisteners;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+
+public class ExtentManager {
+
+	private static ExtentReports extent;
+	
+	
+	
+
+	    public static ExtentReports createInstance(String fileName) {
+	        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
+	       
+	        
+	        htmlReporter.config().setTheme(Theme.STANDARD);
+	        htmlReporter.config().setDocumentTitle(fileName);
+	        htmlReporter.config().setEncoding("utf-8");
+	        htmlReporter.config().setReportName(fileName);
+	        
+	        extent = new ExtentReports();
+	        extent.attachReporter(htmlReporter);
+	        extent.setSystemInfo("Automation Tester", "Swapna");
+	        extent.setSystemInfo("Organization", "Iteration Matrix");
+	        extent.setSystemInfo("Build no", "IN-Ver-1.001");
+	        
+	        
+	        return extent;
+	    }
+
+	    
+	  /*  public static String screenshotPath;
+		public static String screenshotName;
+		
+		public static void captureScreenshot() {
+
+			File scrFile = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
+
+			Date d = new Date();
+			screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+
+			try {
+				FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir") + "\\reports\\" + screenshotName));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		
+		}*/
+	
+
+	}
